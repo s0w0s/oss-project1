53e5b0972010351b6ecd20213d721a4436866a0c
s0w0s <jangwooseng97@gmail.com>
2019-04-10 23:11:46 +0900

    reset

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -2,16 +2,8 @@
 
 int main()
 {
-  int i = 0;
-
   printf("First oss code\n");
   printf("Third oss code\n");
-
-  printf("마스터 브랜치에 코드를 추가하였음\n", );
-
-  for(int i= 0; i<10 i ++){
-    prinf("test 브랜치\n");
-  }
 
   return 0;
 }
