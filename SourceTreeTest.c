bbfe4decc5676ac252328eb403fe34acf39ae8ef
s0w0s <jangwooseng97@gmail.com>
2019-04-10 23:06:50 +0900

    Merge branch 'test'
    # Conflicts:
    #	SourceTreeTest.c

--- SourceTreeTest.c
+++ SourceTreeTest.c
@@ -7,8 +7,11 @@ int main()
   printf("First oss code\n");
   printf("Third oss code\n");
 
-  for(int i=0;i<10;i++){
-    prinf("Test 브랜치\n");
+  printf("마스터 브랜치에 코드를 추가하였음\n", );
+
+  for(int i= 0; i<10 i ++){
+    prinf("test 브랜치\n");
   }
+
   return 0;
 }
